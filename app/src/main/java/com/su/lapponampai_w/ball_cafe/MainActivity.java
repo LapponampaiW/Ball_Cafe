15cb4612b222fd3f9a5048f91134ab479637fe86
ball <ballz_v@hotmail.com>
2016-04-17 07:37:51 +0700

    first page almost finish

---
+++ app/src/main/java/com/su/lapponampai_w/ball_cafe/MainActivity.java
@@ -0,0 +1,84 @@
+package com.su.lapponampai_w.ball_cafe;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonlogin;
+    EditText userEditText, passwordEditText;
+    MyManage myManage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myManage = new MyManage(this);
+
+        //myManage.addValue(1, "ballzv", "1234", "Weerachod");
+
+        bingwidget();
+
+        buttonlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String userString = userEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                if (userString.equals("") || passwordString.equals("")) {
+
+
+                    myManage.errorDialog(MainActivity.this, "Ball Cafe", "กรุณากรอกข้อมูลให้ครบถ้วน");
+
+                } else {
+
+                    checkUserNamePassword(userString, passwordString);
+
+
+                }
+
+
+
+
+            }
+        });
+
+    }
+
+
+    private void checkUserNamePassword(String userString, String passwordString) {
+        String[] strings = myManage.searchUserNamePassword(userString);
+
+        try {
+
+            if (passwordString.equals(strings[2])) {
+                Toast t = Toast.makeText(MainActivity.this, "สำเร็จแล้ว", Toast.LENGTH_LONG);
+                t.show();
+            } else {
+                Toast t = Toast.makeText(MainActivity.this, "Username or Password Incorrect Please Check Again", Toast.LENGTH_LONG);
+                t.show();
+            }
+
+        } catch (Exception e) {
+            Toast t = Toast.makeText(MainActivity.this, "Exception e", Toast.LENGTH_LONG);
+            t.show();
+        }
+    }
+
+    private void bingwidget() {
+        buttonlogin = (Button) findViewById(R.id.buttonLogin);
+        userEditText = (EditText) findViewById(R.id.userEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+    }
+
+
+}
---
+++ app/src/main/java/com/su/lapponampai_w/ball_cafe/MyManage.java
@@ -0,0 +1,98 @@
+package com.su.lapponampai_w.ball_cafe;
+
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by apple on 4/16/16.
+ */
+public class MyManage {
+
+    MySQLiteOpenHelper openHelper;
+    SQLiteDatabase readSqLiteDatabase, writeSqLiteDatabase;
+    String[] columnUser = {"_id","User","Password","Name"};
+
+    public MyManage(Context context) {
+
+
+        openHelper = new MySQLiteOpenHelper(context);
+        readSqLiteDatabase = openHelper.getReadableDatabase();
+        writeSqLiteDatabase = openHelper.getWritableDatabase();
+
+    }
+
+    public long addValue(int intTable,
+                         String strColumn2,
+                         String strColumn3,
+                         String strColumn4) {
+        ContentValues contentValues = new ContentValues();
+
+        long mylong = 0;
+
+        switch (intTable) {
+            case 1:
+                contentValues.put("User",strColumn2);
+                contentValues.put("Password",strColumn3);
+                contentValues.put("Name",strColumn4);
+
+                mylong = writeSqLiteDatabase.insert("userTABLE", null, contentValues);
+
+                break;
+            default:
+                break;
+
+        }
+        return mylong;
+    }
+    //ทำการ filter ได้สำเร็จ
+    public Cursor filter(String i) {
+        Cursor cursor = readSqLiteDatabase.query("userTABLE", columnUser , "User = " + i +";", null, null, null, null);
+        return cursor;
+    }
+
+    public void errorDialog(Context context,String strTitle, String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setIcon(R.drawable.icon_question);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setCancelable(false);
+        builder.setPositiveButton("ตกลง", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
+    }
+
+
+    public String[] searchUserNamePassword(String strUser) {
+        try {
+            String[] strResult = null;
+           Cursor cursor = readSqLiteDatabase.query("userTABLE", columnUser , "User =?", new String[]{String.valueOf(strUser)}, null, null, null);
+            //Cursor cursor = readSqLiteDatabase.query("userTABLE", columnUser , "User = " + strUser +";", null, null, null, null);
+            if (cursor != null) {
+                if (cursor.moveToFirst()) {
+                    strResult = new String[4];
+
+                    strResult[0] = cursor.getString(0);
+                    strResult[1] = cursor.getString(1);
+                    strResult[2] = cursor.getString(2);
+                    strResult[3] = cursor.getString(3);
+                }
+                cursor.close();
+                return strResult;
+            }
+
+        } catch (Exception e) {
+            return null;
+        }
+        return null;
+    }
+}
